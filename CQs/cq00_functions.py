325dd2be872f23e30fe33b79dc839016451707e1
Akshita Padala <akpadala@unc.edu>
2024-09-02 20:33:10 -0400

    Finished Exercise 00.Attempt 1

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,13 @@
+"""My first CQ!"""
+
+__author__ = "730745996"
+def mimic(message: str) -> str:
+    """This function will repeat a string back to you"""
+    return message
+
+def main():
+    """This function will ask you for a message and then repeat it back to you"""
+    return print(mimic(message=input("What is your message")))
+
+if__name__ == "__main__":
+    main()
